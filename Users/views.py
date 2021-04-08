01581c44b4e676f2749f8e469241127d7ee3ee10
Euhid Aman <aman.derax20@gmail.com>
2021-04-08 23:45:58 +0530

    Added link to login and logout frm frontend

--- Users/views.py
+++ Users/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # username - Aman
 # password - password
 
+# https://realpython.com/django-user-management/#change-passwords
+
 # Create your views here.
 def dashboard(request):
     return render(request, 'Users/dashboard.html')
